91eab7b070231ed2a6cfa728136ec5ba1383eb69
goodook <goodook@163.com>
2018-05-25 22:11:24 +0800

    utf8

--- doc/sql/admineap.sql
+++ doc/sql/admineap.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+﻿-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
 --
 -- Host: 127.0.0.1    Database: admineap
 -- ------------------------------------------------------
